cf8b54a13220e05b87b8253f8b4edd6cc4524163
LaurynasSavickas <93864551+LaurynasSavickas@users.noreply.github.com>
2021-12-12 09:59:21 +0200

    katologai

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function doSomething(){    
+    let elem = document.getElementById('my-div');
+    elem.style.border = '1px solid black'
+    elem.style.padding = '10px';
+    elem.style.margin = '10px';
+}
