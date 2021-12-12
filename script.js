96d5b580b1608ab9d3541182762a0e55776032b3
LaurynasSavickas <93864551+LaurynasSavickas@users.noreply.github.com>
2021-12-12 11:41:57 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function doSomething(){    
-    let elem = document.getElementById('my-div');
-    elem.style.border = '1px solid black'
-    elem.style.padding = '10px';
-    elem.style.margin = '10px';
-}
